d38e6023d57677ef78f10cde4b4180a25c109a28
Gupta <saloni.gupta@sap.com>
2021-05-13 21:26:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.use(express.json());
+app.use(express.urlencoded());
+app.get("/", function (req, res) {
+    const homepage = fs.readFileSync("./index.html",{"encoding":"utf-8"});
+    res.send(homepage);
+});
+app.get("/abc.jpg", function (req, res) {
+    const homepage = fs.readFileSync("./abc.jpg");
+    res.send(homepage);
+});
+app.post("/read-task",function(req,res){
+   
+    const data = fs.readFileSync("./task.json",{"encoding":"utf-8"});
+
+    res.send(data);
+})
+
+
+app.post("/update-task",function(req,res){
+   
+    const data = fs.writeFileSync("./task.json",JSON.stringify(req.body));
+
+    res.send(JSON.stringify(data));
+})
+app.listen(3008,function(){
+  console.log("my app is ready");
+})
+
